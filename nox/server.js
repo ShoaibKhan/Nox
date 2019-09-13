0530ff9dadd9dc449671d196e426a64f21f9bc2a
Shoaib Khan <shoaiba.khan@mail.utoronto.ca>
2019-09-13 12:06:26 -0400

    Generic client created and react app started

--- nox/server.js
+++ nox/server.js
@@ -15,7 +15,7 @@ const db = require('./config/keys').mongoURI;
 //Connect to Mongo
 mongoose
     .connect(db, {useNewUrlParser: true, useCreateIndex: true})
-    .then(() => console.log('Connected to Server!'))
+    .then(() => console.log('MongoDB Connected!'))
     .catch(err => console.log(err));
 
 //Use Routes
