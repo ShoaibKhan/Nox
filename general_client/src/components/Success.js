dcad9804bf8afaef5a8ed81113f80736d714631f
Shoaib Khan <shoaiba.khan@mail.utoronto.ca>
2019-10-16 13:56:10 -0400

    MultiTheme requirement for Material-UI removed

--- general_client/src/components/Success.js
+++ general_client/src/components/Success.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import AppBar from '@material-ui/core/AppBar';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
 export class Success extends Component {
   continue = e => {
@@ -17,19 +16,15 @@ export class Success extends Component {
 
   render() {
     return (
-      <MuiThemeProvider > 
-        <React.Fragment>
-        <Dialog 
-            open="true"
-            fullWidth="true"
-            maxWidth='sm'
-          >
-            <AppBar title="Success" />
-            <h4>Session Created!</h4>
-            <p>Thank you "Insert Professor Here"</p>
-          </Dialog>
-        </React.Fragment>
-      </MuiThemeProvider>
+      <Dialog
+        open="true"
+        fullWidth="true"
+        maxWidth='sm'
+      >
+        <AppBar title="Success" />
+        <h4>Session Created!</h4>
+        <p>Thank you "Insert Professor Here"</p>
+      </Dialog>
     );
   }
 }
