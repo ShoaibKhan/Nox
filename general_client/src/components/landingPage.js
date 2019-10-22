81e9e555455affebe96827c01cbf5aea45c229f8
Ali <root@DESKTOP-O21TJ63.localdomain>
2019-10-22 16:55:03 -0400

    Centered home page component

--- general_client/src/components/landingPage.js
+++ general_client/src/components/landingPage.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import InputGroup from 'react-bootstrap/InputGroup';
-import { Button, FormControl, Container } from "react-bootstrap";
+import { Button, FormControl, Container, Row, Col } from "react-bootstrap";
 import Image from 'react-bootstrap/Image'
 import NoxLogo from '../images/noxLogo.png'
 import axios from 'axios';
+
 import UserForm from './UserForm';
 
 axios.defaults.withCredentials = true
@@ -73,33 +74,34 @@ export default class landingPage extends Component {
 
     render() {
         return (
-            <div>
-                <Container style={{ width: 300 }}>
-                    <div style={{ position: 'relative', right: 65 }}>
-                        <Image src={NoxLogo} alt='Nox Logo' className='logo' />
-                    </div>
-                    <InputGroup className="md">
-                        <FormControl
-                            ref={this.codeBox}
-                            placeholder="Session ID"
-                            aria-label="Session ID"
-                            aria-describedby="basic-addon2"
-                        />
-                    </InputGroup>
 
-                    <Button style={{ width: 270 }} variant="dark" onClick={this.onJoinSession}>Enter</Button>
+
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '65vh' }}>
+
+
+
+
+                <Container style={{ width: 300 }}>
+                    <Row className="show-grid">
+
+                        <div style={{ position: 'relative', right: 50 }}>
+                            <Image src={NoxLogo} alt='Nox Logo' />
+                        </div>
+                        <InputGroup >
+                            <FormControl
+                                ref={this.codeBox}
+                                placeholder="Session ID"
+                                aria-label="Session ID"
+                                aria-describedby="basic-addon2"
+                            />
+                        </InputGroup>
+
+                        <Button style={{ width: 300 }} variant="dark" onClick={this.onJoinSession}>Enter</Button>
+                    </Row>
                 </Container>
-                <div >
-                    <div style={phantom} />
-                    <div style={style}>
-                        <Button style={{ width: 270 }} variant="dark" onClick={this._onButtonClick}> Professor? Host a Session!</Button>
-                        {this.state.showComponent ?
-                            <UserForm /> :
-                            null
-                        }
-                    </div>
-                </div>
+
             </div>
+
         );
     }
 }
